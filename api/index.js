2b0ba310fc85aed4ac7a4370aaf4f47923889025
Fenil05 <fenilgajjar2265@gmail.com>
2024-04-06 15:15:01 +0530

    complete sign page functionality

--- api/index.js
+++ api/index.js
@@ -3,10 +3,13 @@ import mongoose from "mongoose"
 import dotenv from "dotenv"
 import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
+import cors from "cors"
 
 const app = express()
 dotenv.config()
 app.use(express.json())
+app.use(cors({origin:"http://localhost:5173",credentials:true}))
+
 
 const connect = async()=>{
     try {
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,34 +1,62 @@
-import React from 'react'
-import {Link} from "react-router-dom"
+import React, { useState } from 'react'
+import {Link,useNavigate} from "react-router-dom"
+import apiRequest from '../utils/apiRequest'
 
 const SignUp = () => {
+  const navigate = useNavigate()
+  const [loading,setLoading] = useState(false)
+  const [error,setError] = useState(null)
+  const [formData,setFormData] = useState({})
+
+  const handleChange = (e) => {
+    setFormData({...formData,[e.target.id]:e.target.value})
+  }
+
+  const handleSubmit = async(e) => {
+    e.preventDefault()
+    setLoading(true)
+    setError(null)
+    try {
+      const res = await apiRequest.post("auth/signup",formData)
+      setLoading(false)
+      navigate("/sign-in")
+    } catch (err) {
+      setError(err.response.data.message)
+      setLoading(false)
+    }
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form className='flex flex-col gap-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
           type='text'
           placeholder='username'
           className='border p-3 rounded-lg'
           id='username'
+          onChange={handleChange}
         />
         <input
           type='email'
           placeholder='email'
           className='border p-3 rounded-lg'
           id='email'
+          onChange={handleChange}
         />
         <input
           type='password'
           placeholder='password'
           className='border p-3 rounded-lg'
           id='password'
+          onChange={handleChange}
         />
 
         <button
+        disabled={loading}
           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
         >
-          Sign Up
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
       <div className='flex gap-2 mt-5'>
@@ -37,7 +65,7 @@ const SignUp = () => {
           <span className='text-blue-700'>Sign in</span>
         </Link>
       </div>
-      {/* {error && <p className='text-red-500 mt-5'>{error}</p>} */}
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
 
   )
