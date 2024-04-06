c830d8f03d3f61f5f48f3535bb2b52f5bf392156
Fenil05 <fenilgajjar2265@gmail.com>
2024-04-06 12:43:51 +0530

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express" 
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import userRouter from "./routes/user.route.js"
 
 const app = express()
 dotenv.config()
@@ -13,6 +14,8 @@ const connect = async()=>{
         console.log(err);
     }
 }
+
+app.use("/api/user",userRouter)
 
 app.listen(3000,()=>{
     connect()
