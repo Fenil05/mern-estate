bc069be7afded033d61b9e5b9a47a76e9922e58c
Fenil05 <fenilgajjar2265@gmail.com>
2024-04-06 18:17:41 +0530

    complete profile page ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,73 @@
 import React from 'react'
+import {Link} from "react-router-dom"
+import {useSelector} from "react-redux"
 
 const Profile = () => {
+  const {currentUser}  = useSelector((state) => state.user)
   return (
-    <div>Profile</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <form className='flex flex-col gap-4'>
+        <input
+          type='file'
+          hidden
+          accept='image/*'
+        />
+        <img
+          src={currentUser.avatar}
+          alt='profile'
+          className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'
+        />
+        <p className='text-sm self-center'>
+          
+        </p>
+        <input
+          type='text'
+          placeholder='username'
+          id='username'
+          className='border p-3 rounded-lg'
+        />
+        <input
+          type='email'
+          placeholder='email'
+          id='email'
+          className='border p-3 rounded-lg'
+        />
+        <input
+          type='password'
+          placeholder='password'
+          id='password'
+          className='border p-3 rounded-lg'
+        />
+        <button
+          className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          Update
+        </button>
+        <Link
+          className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'
+          to={'/create-listing'}
+        >
+          Create Listing
+        </Link>
+      </form>
+      <div className='flex justify-between mt-5'>
+        <span
+          className='text-red-700 cursor-pointer'
+        >
+          Delete account
+        </span>
+        <span className='text-red-700 cursor-pointer'
+        >
+          Sign out
+        </span>
+      </div>
+      <button 
+      className='text-green-700 w-full'>Show Listings</button>
+      <p className='text-red-700 mt-5'></p>
+      
+    </div>
+
   )
 }
 
