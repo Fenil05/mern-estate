22df1c4fd82cda186c1515acf52fd988df6b50c8
Fenil05 <fenilgajjar2265@gmail.com>
2024-04-06 15:32:53 +0530

    create signin api route

--- api/index.js
+++ api/index.js
@@ -4,11 +4,13 @@ import dotenv from "dotenv"
 import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
 import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express()
 dotenv.config()
 app.use(express.json())
 app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use(cookieParser())
 
 
 const connect = async()=>{
